3d7faaf479065c80428cca851deda02d2d521fb9
network7277 <husanjurayev7277@gmail.com>
2023-02-13 20:54:45 +0500

    var08

--- bin/var08.dart
+++ bin/var08.dart
@@ -6,4 +6,8 @@ Create a string  variable that contains  letters and numbers
 
 void main() {
   // write your code here
+  String satrli;
+  satrli = 'men 2004-yil 18-avgustda tug\'ilganman';
+  print(satrli);
+  
 }
